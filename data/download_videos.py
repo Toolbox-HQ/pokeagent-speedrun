7dae7187f60cb931a94ab49bda5e987cd872cbb1
Ben Schneider <schnei19@myumanitoba.ca>
2025-09-11 03:00:05 +0000

    download videos in parallel

--- data/download_videos.py
+++ data/download_videos.py
@@ -5,6 +5,7 @@ from typing import List
 import json
 from tqdm import tqdm
 import os
+import joblib
 
 
 def list_files(directory):
@@ -13,28 +14,43 @@ def list_files(directory):
     ]
 
 
-def download_videos(youtube_ids: List[str], output_dir: str):
-    cached_videos = [file.split(".")[0] for file in list_files(output_dir)]
+def download_video(youtube_id: str, output_dir: str):
+    url = f"https://www.youtube.com/watch?v={youtube_id}"
+    yt = YouTube(url, on_progress_callback=on_progress)
+    ys = yt.streams.get_highest_resolution()
+    ys.download(output_path=output_dir, filename=f"{youtube_id}.mp4")
 
-    for id in tqdm(youtube_ids):
-        if id not in cached_videos:
-            url = f"https://www.youtube.com/watch?v={id}"
-            yt = YouTube(url, on_progress_callback=on_progress)
-            ys = yt.streams.get_highest_resolution()
-            ys.download(output_path=output_dir, filename=f"{id}.mp4")
-            cached_videos.append(id)
+
+def download_videos_in_parallel(youtube_ids: List[str], output_dir: str, threads: int = 1):
+    cached_videos = [file.split(".")[0] for file in list_files(output_dir)]
+    ids_to_download = [id for id in youtube_ids if id not in cached_videos]
+
+    if not ids_to_download:
+        print("All videos are already cached.")
+        return
+
+    with tqdm(total=len(ids_to_download)):
+        joblib.Parallel(n_jobs=threads)(
+            joblib.delayed(download_video)(id, output_dir) for id in ids_to_download
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str)
     parser.add_argument("--output_dir", default="./cache/video", type=str)
+    parser.add_argument("--threads", default=1, type=int)
 
     args = parser.parse_args()
+
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
 
     with open(args.input, "r") as f:
         metadata = json.load(f)
 
     ids = [item["search_result"]["id"]["videoId"] for item in metadata]
 
-    download_videos(ids, args.output_dir)
+    # Use the joblib-wrapped function
+    download_videos_in_parallel(ids, args.output_dir, threads=args.threads)
