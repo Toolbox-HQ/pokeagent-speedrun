711dcc31a10bf3b8df5a30372096a389d08a80bb
Ben Schneider <schnei19@myumanitoba.ca>
2025-10-01 02:40:48 +0000

    update

--- script/run_agent.sh
+++ script/run_agent.sh
@@ -5,8 +5,8 @@ STATE_NUM=$((RANDOM % 10))
 
 python main.py \
     --max-steps 100000 \
-    --save-s3 "random_movement_policy" \
-    --policy "random_movement_policy" \
+    --save-s3 "test_filter" \
+    --policy "random_policy" \
     --save-state "./state/${STATE_NUM}.state" \
     --keys-json-path "./Data/keys_${RND}.json" \
     --mp4-path "./Data/output_${RND}.mp4"
