92ec3acb647315dd41430276216a15a73ab9d997
ben-schneider-code <schnei19@myumanitoba.ca>
2025-10-05 16:16:12 -0400

    update

--- script/run_agent.sh
+++ script/run_agent.sh
@@ -4,7 +4,7 @@ RND=$(uuidgen | tr '[:upper:]' '[:lower:]' | cut -c1-8)
 STATE_NUM=$((RANDOM % 10))
 
 python main.py \
-    --save-s3 "emulatorv2" \
+    --save-s3 $2 \
     --policy $1 \
     --save-state "./state/${STATE_NUM}.state" \
     --keys-json-path "./Data/keys_${RND}.json" \
