c05a79cc321faa39942564146890538cd4b02837
Xavier Schneider <xmanperson@gmail.com>
2025-11-10 13:57:48 -0500

    fix manual_mode script

--- script/manual_mode.sh
+++ script/manual_mode.sh
@@ -1,4 +1,4 @@
-source ./.venv/bin/activate
+source .venv/bin/activate
 
 RND=$(uuidgen | tr '[:upper:]' '[:lower:]' | cut -c1-8)
 
@@ -6,4 +6,5 @@ python -m emulator.emulator_client \
     --keys-json-path "./emulator/data/keys_${RND}.json" \
     --mp4-path "./emulator/data/output_${RND}.mp4" \
     --manual-mode \
-    --fps 60
+    --fps 60 \
+    --save-state "./emulator/agent_direct_save.state"
