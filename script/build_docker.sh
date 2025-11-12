741a65d4dc87e906740b41811230fac4faaed877
ben-schneider-code <schnei19@myumanitoba.ca>
2025-11-12 16:09:50 -0500

    update

--- script/build_docker.sh
+++ script/build_docker.sh
@@ -7,5 +7,7 @@ BUILD_DIR="./.cache/pokeagent/containers"
 TAG="latest"
 
 docker build -f dconfig/Dockerfile -t "$IMAGE_NAME:$TAG" .
+
+mkdir -p $BUILD_DIR
 docker save "$IMAGE_NAME:$TAG" -o "$BUILD_DIR/${IMAGE_NAME}_${TAG}.tar"
-echo "Docker image built!"
+echo ""$IMAGE_NAME:$TAG"  saved to $BUILD_DIR/${IMAGE_NAME}_${TAG}.tar"
