45f8e856ccdda826916c66fa3bc5fec0304b7da8
Ben Schneider <schnei19@myumanitoba.ca>
2025-11-12 18:17:50 +0000

    - move config to be top level - update docker scripts

--- script/run_agent_docker.sh
+++ script/run_agent_docker.sh
@@ -3,8 +3,9 @@ set -e
 
 IMAGE_NAME="pokeagent"
 TAG="latest"
+HF_DIR="${HF_HOME:-$HOME/.cache/huggingface}"
 
-docker run --name agent_run --gpus all "$IMAGE_NAME:$TAG"
+docker run --mount type=bind,src=./.cache,dst=/app/.cache --mount type=bind,src=$HF_DIR,dst=/hf_cache --gpus all -e HF_HOME=/hf_cache "$IMAGE_NAME:$TAG" 
 docker cp agent_run:/app/emulator/data/output.mp4 ./.cache/output.mp4
 docker rm -f agent_run
 
