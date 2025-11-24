4b426a77942af4cd47fdfb4ed9dda035064c5a96
ben-schneider-code <schnei19@myumanitoba.ca>
2025-11-24 12:26:20 -0500

    add download script

--- models/util/misc.py
+++ models/util/misc.py
@@ -6,3 +6,13 @@ def local_model_map(model_name: str):
         "google/siglip-base-patch16-224": f"{hf_dir}/hub/models--google--siglip-base-patch16-224/snapshots/7fd15f0689c79d79e38b1c2e2e2370a7bf2761ed",
         "Qwen/Qwen3-1.7B": f"{hf_dir}/hub/models--Qwen--Qwen3-1.7B/snapshots/70d244cc86ccca08cf5af4e1e306ecf908b1ad5e",
     }[model_name]
+
+def download_models():
+    from transformers import AutoModel, AutoProcessor
+    AutoModel.from_pretrained("google/siglip-base-patch16-224")
+    AutoModel.from_pretrained("Qwen/Qwen3-1.7B")
+    AutoProcessor.from_pretrained("google/siglip-base-patch16-224")
+    AutoProcessor.from_pretrained("Qwen/Qwen3-1.7B")
+
+if __name__ == "__main__":
+    download_models()
