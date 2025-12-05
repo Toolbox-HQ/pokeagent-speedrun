716ec6ee200821d0c654ca3de999ce20380e8186
ben-schneider-code <schnei19@myumanitoba.ca>
2025-12-04 22:21:19 -0500

    update run script

--- script/pokeagent_dev.sh
+++ script/pokeagent_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=7:00:00
+#SBATCH --time=16:00:00
 #SBATCH --gpus-per-node=1
 #SBATCH --job-name=agent_inference_job
 #SBATCH --output=/scratch/%u/slurm_out/%j_agent_inference_job_output.txt
