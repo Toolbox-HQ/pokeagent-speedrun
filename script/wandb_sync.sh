78a723990540fee58a22c7d3409119b709736e2e
ben-schneider-code <schnei19@myumanitoba.ca>
2026-03-03 14:48:01 -0500

    cache

--- script/wandb_sync.sh
+++ script/wandb_sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source .venv/bin/activate
 #wandb beta sync --skip-synced .cache/pokeagent/tmp/wandb/offline-run-*
-wandb beta sync --skip-synced ./wandb/wandb/offline-run-*
+wandb beta sync --skip-synced /scratch/bsch/pokeagent-speedrun/wandb/wandboffline-run-*
