fd92e14d8e57c003f67ca974834ed4092abf2b78
ben-schneider-code <schnei19@myumanitoba.ca>
2026-03-06 18:00:58 -0500

    update

--- script/dino_embed.sh
+++ script/dino_embed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=192
+#SBATCH --ntasks-per-node=96
 #SBATCH --time=24:00:00
 #SBATCH --job-name=dino_agent_emb
 #SBATCH --output=/scratch/%u/slurm_out/%j_dinov2_inference_job_output.txt
@@ -8,4 +8,4 @@
 
 source p
 export PYTHONPATH=$(pwd)
-python ./models/inference/dino_embed.py
+python ./models/inference/dino_embed.py $1 $2
