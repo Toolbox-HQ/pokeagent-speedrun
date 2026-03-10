29b10153c2eb599c3cab6dcfa5c47990baa7cb14
Benjamin Schneider <benjamin.schneider@uwaterloo.ca>
2026-03-10 14:06:52 -0400

    update manual mode script

--- script/manual_mode.sh
+++ script/manual_mode.sh
@@ -4,7 +4,9 @@ source ./.venv/bin/activate
 RND=$(uuidgen | tr '[:upper:]' '[:lower:]' | cut -c1-8)
 
 python -m emulator.emulator_manual_client \
-    --keys-json-path "./.cache/keys_${RND}.json" \
-    --mp4-path "./.cache/output_${RND}.mp4" \
+    --keys-json-path "./.cache/agent_eval/${RND}.json" \
+    --mp4-path "./.cache/agent_eval/${RND}.mp4" \
     --manual-mode \
-    --fps 60
+    --fps 60 \
+    --save-state $1 \
+    --rom .cache/rom.gba
