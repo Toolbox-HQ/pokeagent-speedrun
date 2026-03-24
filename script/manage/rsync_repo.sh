af1f5aa25141fce26bf76614b751bd2ed1f728b4
ben-schneider-code <schnei19@myumanitoba.ca>
2026-03-23 20:57:10 -0400

    update rsync script

--- script/manage/rsync_repo.sh
+++ script/manage/rsync_repo.sh
@@ -30,20 +30,3 @@ for pat in "${SKIP_ONLY[@]}"; do
 done
 
 rsync "${RSYNC_ARGS[@]}" "$SRC/" "$DST/"
-
-for dir in "${EXCLUDES[@]}"; do
-    src_dir="$SRC/$dir"
-    dst_link="$DST/$dir"
-
-    [ -e "$src_dir" ] || continue
-
-    if [ -L "$dst_link" ]; then
-        rm "$dst_link"
-    elif [ -e "$dst_link" ]; then
-        echo "Warning: $dst_link exists and is not a symlink, skipping"
-        continue
-    fi
-
-    ln -s "$src_dir" "$dst_link"
-    echo "Linked: $dst_link -> $src_dir"
-done
