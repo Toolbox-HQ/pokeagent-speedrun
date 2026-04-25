86ed60e2bbe66992cfc0c6f1e5180b7f736db8d8
ben-schneider-code <schnei19@myumanitoba.ca>
2026-04-25 12:50:53 -0400

    - add hack to support safetensors weight format

--- models/inference/idm_inference_dataloader.py
+++ models/inference/idm_inference_dataloader.py
@@ -22,7 +22,10 @@ AGENT_WINDOW = 64
 
 def load_model(chkpt=".cache/pokeagent/rnd_idm_model.pt"):
     m = IDModel(output_classes=NUM_ACTION_CLASSES)
-    if chkpt is not None:
+    if chkpt.endswith(".safetensors"):
+        from safetensors.torch import load_file
+        m.load_state_dict(load_file(chkpt, device="cpu"))
+    else:
         m.load_state_dict(torch.load(chkpt, map_location="cpu"))
     m.eval()
     return m
